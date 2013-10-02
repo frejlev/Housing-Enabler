20f349a46378c8266958c3c724faee9222180e01
frejlev <andreas@rytman.dk>
2013-10-02 17:42:16 +0200

    afr changes

--- src/ucn/datamatiker/afr_rmlp/housingenabler/MainMenu.java
+++ src/ucn/datamatiker/afr_rmlp/housingenabler/MainMenu.java
@@ -1,8 +1,13 @@
 package ucn.datamatiker.afr_rmlp.housingenabler;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ListView;
 
 public class MainMenu extends Activity {
 
@@ -10,6 +15,23 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		
+		ListView listMenu = (ListView)findViewById(R.id.listMenu);
+		
+		LayoutParams lp = (LayoutParams)listMenu.getLayoutParams();
+		
+		final ArrayList<String> menuItems = new ArrayList<String>();
+		final ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuItems);
+		listMenu.setAdapter(aa);
+		
+		menuItems.add("Opret ny sag");
+		menuItems.add("Opret ny klient");
+		menuItems.add("S¿g i sager");
+		menuItems.add("S¿g i klienter");
+		menuItems.add("Ikke f¾rdiggjorde sager");
+		
+		aa.notifyDataSetChanged();
+		
 	}
 
 	@Override
